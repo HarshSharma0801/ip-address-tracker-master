5ccae8172b0ffe67f8b9fb0708eb1a8579b17428
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-06-04 02:04:49 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const url = 'https://geo.ipify.org/api/v2/country?apiKey=at_6dkW9TSUmdjFxPXIJF2mN2YuOBBwc&ipAddress=8.8.8.8';
+const Key = 'at_6dkW9TSUmdjFxPXIJF2mN2YuOBBwc';
+const options = {
+  Headers:{
+    'Access-Control-Allow-Origin':'*',
+  }
+}
+fetch(url,options)
+.then(response=>response.json())
+.then(data=>console.log(data))
+.catch(error=>alert("Enter a Valid IP Address"));
+
+
 

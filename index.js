563923d3c93e8f1abd5ebb47eb2b3e64585ce43d
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-06-04 04:16:25 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-const url = 'https://geo.ipify.org/api/v2/country?apiKey=at_6dkW9TSUmdjFxPXIJF2mN2YuOBBwc&ipAddress=8.8.8.8';
+
+const IP = document.getElementById("Ip");
+const location1 = document.getElementById("location-region");
+const location2 = document.getElementById("location-country");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
+
+const button = document.getElementById("btn");
+
+
+// const url = 
 const Key = 'at_6dkW9TSUmdjFxPXIJF2mN2YuOBBwc';
 const options = {
   Headers:{
     'Access-Control-Allow-Origin':'*',
   }
 }
-fetch(url,options)
-.then(response=>response.json())
-.then(data=>console.log(data))
-.catch(error=>alert("Enter a Valid IP Address"));
+
+ 
+  function getData(Address){
+    fetch('https://geo.ipify.org/api/v2/country?apiKey=at_6dkW9TSUmdjFxPXIJF2mN2YuOBBwc&ipAddress='+Address,options)
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      IP.innerHTML=Address
+      location1.innerHTML=data.location.region
+      location2.innerHTML=data.location.country
+      isp.innerHTML=data.isp
+      timezone.innerHTML=data.location.timezone
+
+  
+    })
+    .catch(error=>alert("Enter a Valid IP Address"));
+  }
+
+  button.addEventListener('click',()=>{
+    getData(searchbar.value);
+  })
+
+
+
+  
+
+
+
+
+
+
+
 
 
 
